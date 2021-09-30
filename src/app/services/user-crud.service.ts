b91d411005e9d1c97249d67b56a7b4887ec1f197
prathamesh-flixdrop <91131757+prathamesh-flixdrop@users.noreply.github.com>
2021-09-30 10:52:23 +0530

    Update user-crud.service.ts

--- src/app/services/user-crud.service.ts
+++ src/app/services/user-crud.service.ts
@@ -25,14 +25,14 @@ export class UserCrudService {
   constructor(private httpClient: HttpClient) { }
 
   createUser(user: User): Observable<any> {
-    return this.httpClient.post<User>('/api/create-user', user, this.httpOptions) //http://localhost:5000/api/
+    return this.httpClient.post<User>('/create-user', user, this.httpOptions) //http://localhost:5000/api/
       .pipe(
         catchError(this.handleError<User>('Error occured'))
       );
   }
 
   getUser(id): Observable<User[]> {
-    return this.httpClient.get<User[]>('/api/fetch-user/' + id)
+    return this.httpClient.get<User[]>('fetch-user' + id)
       .pipe(
         tap(_ => console.log(`User fetched: ${id}`)),
         catchError(this.handleError<User[]>(`Get user id=${id}`))
@@ -48,7 +48,7 @@ export class UserCrudService {
   }
 
   updateUser(id, user: User): Observable<any> {
-    return this.httpClient.put('/api/update-user/' + id, user, this.httpOptions)
+    return this.httpClient.put('/update-user' + id, user, this.httpOptions)
       .pipe(
         tap(_ => console.log(`User updated: ${id}`)),
         catchError(this.handleError<User[]>('Update user'))
@@ -56,7 +56,7 @@ export class UserCrudService {
   }
 
   deleteUser(id): Observable<User[]> {
-    return this.httpClient.delete<User[]>('/api/delete-user/' + id, this.httpOptions)
+    return this.httpClient.delete<User[]>('/delete-user/' + id, this.httpOptions)
       .pipe(
         tap(_ => console.log(`User deleted: ${id}`)),
         catchError(this.handleError<User[]>('Delete user'))
