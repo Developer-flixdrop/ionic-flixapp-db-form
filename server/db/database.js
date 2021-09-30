f072c4afb1f9170337ba5639f773abfe5ff6b763
prathamesh-flixdrop <91131757+prathamesh-flixdrop@users.noreply.github.com>
2021-09-30 10:02:14 +0530

    Update database.js

--- server/db/database.js
+++ server/db/database.js
@@ -1,4 +1,5 @@
 module.exports = {
   //db: 'mongodb://localhost:27017/mongodb'
-  db: 'mongodb+srv://flixDbUser:itfH69KJMeqtuar@cluster0.m3jjt.mongodb.net/test'
+  //db: 'mongodb+srv://flixDbUser:itfH69KJMeqtuar@cluster0.m3jjt.mongodb.net/test'
+  db: 'mongodb+srv://flixDbUser:itfH69KJMeqtuar@cluster0.m3jjt.mongodb.net/test?retryWrites=true&w=majority'
 };
